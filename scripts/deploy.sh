c2732532cc45ceb0060e1e6917a5acddd9bc904b
joon0329 <jhj9903@naver.com>
2025-12-19 16:22:25 +0900

    fix: correct jar path for CodeDeploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
-# scripts/deploy.sh
 #!/bin/bash
-set -e
 
 APP_DIR=/home/ec2-user/app
-JAR_NAME=$(ls $APP_DIR/build/libs/*.jar | head -n 1)
+JAR_NAME=$(find $APP_DIR -name "*.jar" | head -n 1)
 
-PID=$(pgrep -f java || true)
+echo "Using jar: $JAR_NAME"
+
+PID=$(pgrep -f java)
 if [ -n "$PID" ]; then
   kill -15 $PID
   sleep 5
 fi
 
-nohup java -jar "$JAR_NAME" > "$APP_DIR/app.log" 2>&1 &
+nohup java -jar $JAR_NAME > $APP_DIR/app.log 2>&1 &
