03ec659746ef29c950d125c4a43309cfe0718408
joon0329 <jhj9903@naver.com>
2025-12-19 14:19:56 +0900

    chore: prepare ci/cd specs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,14 @@
+# scripts/deploy.sh
 #!/bin/bash
+set -e
 
 APP_DIR=/home/ec2-user/app
-JAR_NAME=$(ls $APP_DIR/*.jar | head -n 1)
+JAR_NAME=$(ls $APP_DIR/build/libs/*.jar | head -n 1)
 
-echo "배포 시작"
-
-PID=$(pgrep -f java)
+PID=$(pgrep -f java || true)
 if [ -n "$PID" ]; then
   kill -15 $PID
   sleep 5
 fi
 
-nohup java -jar $JAR_NAME > $APP_DIR/app.log 2>&1 &
-## test ##
+nohup java -jar "$JAR_NAME" > "$APP_DIR/app.log" 2>&1 &
