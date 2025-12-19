d9c6d4c2ccdc05070e2cfc2273264ae9c7749ea2
joon0329 <jhj9903@naver.com>
2025-12-19 11:42:39 +0900

    fix: deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 APP_DIR=/home/ec2-user/app
-JAR_NAME=$(ls $APP_DIR/build/libs/*.jar | head -n 1)
+JAR_NAME=$(ls $APP_DIR/*.jar | head -n 1)
 
 echo "배포 시작"
 
-# 기존 실행 중인 앱 종료
 PID=$(pgrep -f java)
 if [ -n "$PID" ]; then
   kill -15 $PID
   sleep 5
 fi
 
-# 새 앱 실행
-nohup java -jar $JAR_NAME > app.log 2>&1 &
-# ///
+nohup java -jar $JAR_NAME > $APP_DIR/app.log 2>&1 &
