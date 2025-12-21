1c8974339045e1e0ed20e2c4a4c3117f76dffb65
joon0329 <jhj9903@naver.com>
2025-12-21 16:04:51 +0900

    feat: deploy.sh fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 APP_DIR=/home/ec2-user/app
-JAVA_BIN=/usr/bin/java
 
 echo "🚀 배포 시작"
 
-JAR_NAME=$(ls $APP_DIR/*.jar | head -n 1)
+# jar를 하위 디렉터리까지 포함해서 찾기
+JAR_NAME=$(find $APP_DIR -name "*.jar" | head -n 1)
+
+echo "JAR = $JAR_NAME"
 
 if [ -z "$JAR_NAME" ]; then
   echo "❌ JAR 파일을 찾지 못함"
   exit 1
 fi
 
-echo "▶ 실행 JAR: $JAR_NAME"
-
-PID=$(pgrep -f "$JAR_NAME")
+PID=$(pgrep -f java)
 if [ -n "$PID" ]; then
-  echo "🔴 기존 프로세스 종료: $PID"
+  echo "기존 프로세스 종료: $PID"
   kill -15 $PID
   sleep 5
 fi
 
-echo "🟢 서버 실행"
-nohup $JAVA_BIN -jar $JAR_NAME > $APP_DIR/app.log 2>&1 &
+nohup java -jar $JAR_NAME > $APP_DIR/app.log 2>&1 &
